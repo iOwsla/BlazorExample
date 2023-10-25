c255bdad2343439f1b16b9b9987cf9c5a7159f8d
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-10-25 23:15:29 +0300

    [Feature]: PasswordTypes Added.
    IncludeNumbers,IncludeLowercase,IncludeUppercase,IncludeSpecial added this project.

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGeneratorApp
+{
+    public class PasswordGenerator
+    {
+        private readonly Random _random;
+        private readonly StringBuilder _passwordBuilder;
+        private readonly StringBuilder _charSetBuilder;
+
+        public PasswordGenerator()
+        {
+            _random = new Random();
+            _passwordBuilder = new StringBuilder();
+            _charSetBuilder = new StringBuilder();
+
+        }
+        public int Length { get; set; }
+        public bool IncludeLowercase { get; set; } = true;
+        public bool IncludeUppercase { get; set; } = true;
+        public bool IncludeNumbers { get; set; } = true;
+        public bool IncludeSpecial { get; set; } = true;
+
+        private readonly string _lowercase = "abcdefghijklmnopqrstuvwxyz";
+        private readonly string _uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private readonly string _numbers = "0123456789";
+        private readonly string _specials = "!@#$%^&*()_+";
+
+        public string Generate()
+        {
+            _passwordBuilder.Clear();
+            string newChars = SetCharBuilder();
+
+            for (int i = 0; i < Length; i++)
+            {
+                _passwordBuilder.Append(newChars[_random.Next(0, newChars.Length)]);
+            }
+
+            return _passwordBuilder.ToString();
+        }
+
+        private string SetCharBuilder()
+        {
+            if (IncludeLowercase) _charSetBuilder.Append(_lowercase);
+            if (IncludeUppercase) _charSetBuilder.Append(_uppercase);
+            if (IncludeNumbers) _charSetBuilder.Append(_numbers);
+            if (IncludeSpecial) _charSetBuilder.Append(_specials);
+            
+            return _charSetBuilder.ToString();
+        }
+
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System;
+using PasswordGeneratorApp;
+
+PasswordGenerator generator = new PasswordGenerator();
+
+Console.WriteLine("Enter the length of the password to generate:");
+string input = Console.ReadLine();
+generator.Length = int.Parse(input);
+string result = generator.Generate();
+
+Console.WriteLine($"Your password is: {result}");
+Console.ReadLine();
